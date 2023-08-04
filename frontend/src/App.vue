139d81005f81e1c3c090d0cec36e297097133aa8
evelync araujo <evyaraujoifc@gmail.com>
2023-08-04 11:45:04 -0300

    Tailwind Setup

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,6 +5,7 @@ import Header from '@/components/template/Header.vue'
 <template>
 <Header></Header>
 <main>Main</main>
+<button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">aaaaa</button>
 <footer>Footer</footer>
 </template>
 
--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,5 +1,7 @@
 <script setup></script>
 
-<template></template>
+<template> 
+
+</template>
 
 <style scoped></style>
