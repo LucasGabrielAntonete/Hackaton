5d3ddc31d49cc9e287ce2c3b28ef575fc0d6a90e
Joao-Vitor-2info2 <joaivitorrm@gmail.com>
2023-08-07 16:11:41 -0300

    header iniciacao

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,10 @@
 <script setup>
-import Header from '@/components/template/Header.vue'
+import HeaderVue from '@/components/HeaderVue.vue'
 </script>
 
 <template>
-<Header></Header>
-<main>Main</main>
-<button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">aaaaa</button>
-<footer>Footer</footer>
+<HeaderVue></HeaderVue>
+
 </template>
 
 <style scoped>
---
+++ frontend/src/components/HeaderVue.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+
+<template>
+    <div class="sticky top-0 bg-mantle shadow-lg ">
+        <div class="container gap-4 py-2 items-center min-w-full"></div>
+    </div>
+</template>
+
