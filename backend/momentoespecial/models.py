a14989696f8b127a5f18798537761d2834944448
LucasGabrielAntonete <lucasantonete@gmail.com>
2023-08-07 21:11:05 -0300

    Criação do Model Damas e suas migrations

--- backend/momentoespecial/models.py
+++ backend/momentoespecial/models.py
@@ -30,3 +30,14 @@ class noivas(models.Model):
 
     def __str__(self):
         return self.nome
+    
+
+class damas(models.Model):
+    nome = models.CharField(max_length=100)
+    descrição = models.CharField(max_length=250)
+    tamanho = models.ForeignKey(tamanho, on_delete=models.CASCADE)    
+
+    def __str__(self):
+        return self.nome
+
+
