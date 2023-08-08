2f4b1d78a82d4f5227d89c6665f0f37537f165aa
LucasGabrielAntonete <lucasantonete@gmail.com>
2023-08-07 21:09:37 -0300

    Criação do model Festa e suas migrations

--- backend/momentoespecial/models.py
+++ backend/momentoespecial/models.py
@@ -5,3 +5,10 @@ class tamanho(models.Model):
     tamanho = models.CharField(max_length=50)
 
 
+class festa(models.Model):
+    nome = models.CharField(max_length=100)
+    descrição = models.CharField(max_length=250)
+    tamanho = models.ForeignKey(tamanho, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nome
