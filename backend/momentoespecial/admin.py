1c0694fcfae62147f083c2b3ce5f263fcd648457
LucasGabrielAntonete <lucasantonete@gmail.com>
2023-08-07 21:12:33 -0300

    Criação da .model na admin.py para que o admin do django possa manipular os dados do banco de dados

--- backend/momentoespecial/admin.py
+++ backend/momentoespecial/admin.py
@@ -1,2 +1,12 @@
 from django.contrib import admin
 
+from .models import tamanho, vestido, festa, debutantes, noivas, damas, pajens
+
+admin.site.register(tamanho)
+admin.site.register(vestido)
+admin.site.register(festa)
+admin.site.register(debutantes)
+admin.site.register(noivas)
+admin.site.register(damas)
+admin.site.register(pajens)
+
