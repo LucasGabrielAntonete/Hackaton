24c05bd04944e60553a37d26b3280004ce7c4041
LucasGabrielAntonete <lucasantonete@gmail.com>
2023-08-08 11:36:42 -0300

    Correção do erro do Rest_framework

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -19,6 +19,12 @@ from django.urls import include,path
 from django.conf import settings
 from django.conf.urls.static import static
 from uploader.router import router as uploader_router
+from rest_framework.routers import DefaultRouter
+
+from momentoespecial.views import produtoViewSet
+
+router = DefaultRouter()
+router.register(r"produto", produtoViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
