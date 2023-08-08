df1c4235d9bb0182cc1b14ea59a29cf904f9f407
LucasGabrielAntonete <lucasantonete@gmail.com>
2023-08-07 21:10:39 -0300

    Criação do Model noivas e suas migrations

--- backend/momentoespecial/models.py
+++ backend/momentoespecial/models.py
@@ -21,3 +21,12 @@ class debutantes(models.Model):
 
     def __str__(self):
         return self.nome
+    
+
+class noivas(models.Model):
+    nome = models.CharField(max_length=100)
+    descrição = models.CharField(max_length=250)
+    tamanho = models.ForeignKey(tamanho, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nome
