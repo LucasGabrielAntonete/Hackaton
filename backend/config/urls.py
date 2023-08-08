43e73afe32ef3e7a08eeae8e320cd8f3642af69a
LucasGabrielAntonete <lucasantonete@gmail.com>
2023-08-08 10:51:09 -0300

    Inserção do sistema de imagens no banco de dados

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -14,7 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from django.contrib import admin
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from uploader.router import router as uploader_router
 
 urlpatterns = [
     path('admin/', admin.site.urls),
