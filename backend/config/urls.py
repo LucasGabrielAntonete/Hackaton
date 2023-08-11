7f60160e375c3a45d11bd13ad103234aa628a7fb
Joao-Vitor-2info2 <joaivitorrm@gmail.com>
2023-08-11 09:44:08 -0300

    urls fix

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -15,22 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include,path
-from django.conf import settings
-from django.conf.urls.static import static
-from uploader.router import router as uploader_router
+from django.urls import include, path
+
 from rest_framework.routers import DefaultRouter
 
 from momentoespecial.views import produtoViewSet
 
 router = DefaultRouter()
-router.register(r"produto", produtoViewSet)
+router.register(r"categorias", produtoViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("api/media/", include(uploader_router.urls)),
-        
+    path("admin/", admin.site.urls),
+    path("", include(router.urls)),
 ]
-
-urlpatterns += static(settings.MEDIA_ENDPOINT, document_root=settings.MEDIA_ROOT)
-
