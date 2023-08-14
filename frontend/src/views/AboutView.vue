558e41f2eb30b02ab9b96039671cebb3fc5361d1
JuliaOliveira26 <julinhalves2006@gmail.com>
2023-08-14 16:54:59 -0300

    Image

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,20 +1,55 @@
-<script setup></script>
+<script setup>
+
+</script>
 
 <template>
     
    <main>
-    <img 
-    class="Momoento"
-    src="https://lh3.googleusercontent.com/p/AF1QipOmcxqHpXR4m24k1cMJ0y10fUDVifJrCHxsuTEy=s680-w680-h510" alt="">
-
+    <div><img 
+    class="Momento"
+    src="./Momento.jpg" alt="">
+</div>
 
    </main>
-    
+     <section >
+        <div class="wrapper">
+<div class="curve curve-top">
+</div>
+<div class="curve curve-bottom">
+</div>
+</div>
         
+
+     </section>
+        
+
 </template>
 <style>
-.Momoento{
-    width:100%;
-    height:1000px;
+.Momento{
+    width: 1950px;
+    height: 600px;
+  
 }
+div.wrapper {
+      background-color:black;
+       width:500px;
+  }
+
+  div.curve {
+    background-color:white;
+    width:500px;
+    height:150px;
+  }
+
+  div.curve-top{   
+      border-bottom-left-radius:70%;
+      border-bottom-right-radius:70%;
+  }
+
+  div.curve-bottom {  
+      margin-top: 50px;
+      border-top-left-radius:70%;
+      border-top-right-radius:70%;
+  }
+
 </style>
