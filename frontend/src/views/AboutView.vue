375ec739134992ed1a404941a0dc8702cebc7f61
JuliaOliveira26 <julinhalves2006@gmail.com>
2023-08-15 16:52:36 -0300

    Hisory

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -8,11 +8,20 @@
     <div><img 
       class="Momento"
     
-    src="./Momento.jpg" alt="">
+    src="./Fachada.png" alt="">
 </div>
+<h1>Quem Somos</h1>
 <section >
-      <div></div>
-      <div class="curve-top"> <h1>Quem Somos</h1></div>
+      
+       
+  <div class="Linha"><div class="Historia">
+
+        <p>Fundado em 2 de maio de 1996, nosso ateliê é especializado em locação de vestidos de noiva, madrinha, 
+          formanda, debutante, dama, primeira comunhão, além de toda a linha de roupas sociais masculinas. 
+          Confeccionamos também toda a linha de vestidos para locação de primeiro uso! Estamos situados na zona sul
+           de Joinville, dispomos de amplo estacionamento e espaço interior, para que você sinta-se muito confortável em estar conosco!"</p>
+       </div></div>     
+  
       
         
 
@@ -23,7 +32,7 @@
 
 </template>
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,400;1,100&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@1,100;1,300;1,400&display=swap');
 
 .Momento{
   width: 1850px;
@@ -32,27 +41,36 @@
 }
 
 h1{
-  text-align: center;
-  font-size: 40px;
-  font-family: 'Lato', sans-serif;
-  font-style:italic;
+  text-align:center;
+ font-size: 40px;
+ font-family: 'Lato', sans-serif;
+ 
   
 }
+.Historia{
+  box-sizing: border-box;
+  float: left;
+  width: 50%;
+  padding: 15px;
+  text-align: center;
+  font-size: 20px;
+  margin-top: 60px;
+}
+.Linha{
+
+  content: "";
+  display:flex;
+  clear: both;
+  
+}
+
 
     
   
 
  
 
-  div.curve-top{   
-
-    width: 1950px;
-     height: 600px;
-     background-color:white;
-     border-top-left-radius:70%;
-      border-top-right-radius:70%;
-  }
-
+ 
   
   
 
