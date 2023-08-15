04725a5d308d7839d6d062743976290f222ed246
LucasGabrielAntonete <lucasantonete@gmail.com>
2023-08-15 10:52:30 -0300

    Inserção do SimpleJWT no arquivo urls.py

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-
 from rest_framework.routers import DefaultRouter
-
 from momentoespecial.views import produtoViewSet
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 router = DefaultRouter()
 router.register(r"produtos", produtoViewSet)
@@ -27,4 +29,6 @@ router.register(r"produtos", produtoViewSet)
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include(router.urls)),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
