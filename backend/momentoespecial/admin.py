9c0d2269778fa709c4e551d1351b3f09d3d3b310
LucasGabrielAntonete <lucasantonete@gmail.com>
2023-08-17 20:45:27 -0300

    Criação do modelo de compra

--- backend/momentoespecial/admin.py
+++ backend/momentoespecial/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import tamanho, categoria, produto
+from .models import tamanho, categoria, produto, Compra
+
 
 admin.site.register(tamanho)
 admin.site.register(categoria)
 admin.site.register(produto)
+admin.site.register(Compra)
--- backend/momentoespecial/models/__init__.py
+++ backend/momentoespecial/models/__init__.py
@@ -1,3 +1,4 @@
 from .categorias import categoria
 from .produto import produto
 from .tamanho import tamanho
+from .compra import Compra
