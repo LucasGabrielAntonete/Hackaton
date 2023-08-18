237a47bb75ed77e1c4942bde6975c4aba4b4022f
LucasGabrielAntonete <lucasantonete@gmail.com>
2023-08-18 11:22:30 -0300

    Criação do itensCompraSerializer

--- backend/momentoespecial/serializers/compra.py
+++ backend/momentoespecial/serializers/compra.py
@@ -1,10 +1,17 @@
 from rest_framework.serializers import ModelSerializer, CharField
 
-from momentoespecial.models import Compra
+from momentoespecial.models import Compra, ItensCompra
+
+
+class ItensCompraSerializer(ModelSerializer):
+    class Meta:
+        model = ItensCompra
+        fields = "__all__"        
 
 class CompraSerializer(ModelSerializer):
     usuario = CharField(source="usuario.email", read_only=True)
     status = CharField(source="get_status_display", read_only=True)
+    itens = ItensCompraSerializer(many=True, read_only=True)
     class Meta:
         model = Compra
         fields = "__all__"
