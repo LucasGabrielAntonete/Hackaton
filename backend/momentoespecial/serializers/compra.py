196faad1bca40fb834b76365c547425698624bb0
LucasGabrielAntonete <lucasantonete@gmail.com>
2023-08-18 11:05:18 -0300

    Inserção de email na listagem de compras para indentificação do usuário

--- backend/momentoespecial/serializers/compra.py
+++ backend/momentoespecial/serializers/compra.py
@@ -3,6 +3,7 @@ from rest_framework.serializers import ModelSerializer, CharField
 from momentoespecial.models import Compra
 
 class CompraSerializer(ModelSerializer):
+    usuario = CharField(source="usuario.email", read_only=True)
     class Meta:
         model = Compra
         fields = "__all__"
