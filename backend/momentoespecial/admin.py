bbd8e42a68df9e258f32b84dbcc01e2659ed172e
LucasGabrielAntonete <lucasantonete@gmail.com>
2023-08-18 10:52:57 -0300

    Adição do compras no admin.py e criação do serializer de compras

--- backend/momentoespecial/admin.py
+++ backend/momentoespecial/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 
-from .models import tamanho, categoria, produto, Compra
+from .models import tamanho, categoria, produto, Compra, ItensCompra
 
 
 admin.site.register(tamanho)
 admin.site.register(categoria)
 admin.site.register(produto)
-admin.site.register(Compra)
+
+
+
+class ItensCompraInline(admin.TabularInline):
+    model = ItensCompra
+
+@admin.register(Compra)
+class CompraAdmin(admin.ModelAdmin):
+    inlines = [ItensCompraInline]
