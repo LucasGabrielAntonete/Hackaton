2f06a4a2ad93b8f3085ab8341f9b74295b0bbd9c
Wecreds <Wecredsbr@gmail.com>
2023-08-25 12:09:51 -0300

    arrumo do admin.py

--- backend/momentoespecial/admin.py
+++ backend/momentoespecial/admin.py
@@ -6,5 +6,11 @@ from .models import tamanho, categoria, produto, Compra, ItensCompra
 admin.site.register(tamanho)
 admin.site.register(categoria)
 admin.site.register(produto)
-admin.site.register(Compra)
 admin.site.register(ItensCompra)
+
+class ItensCompraInline(admin.TabularInline):
+    model = ItensCompra
+
+@admin.register(Compra)
+class CompraAdmin(admin.ModelAdmin):
+    inlines = [ItensCompraInline]
