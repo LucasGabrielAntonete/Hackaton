30dc3a6590913c3e6356813edd4e879a76d84a0e
Wecreds <Wecredsbr@gmail.com>
2023-08-25 18:09:00 -0300

    arrumando imagens do banco

--- frontend/src/components/HeaderVue.vue
+++ frontend/src/components/HeaderVue.vue
@@ -29,10 +29,12 @@ onMounted(() => {
 <template>
   <header>
     <div class="header">
-      <div class="logo">
+      <RouterLink :to="{ name: 'Home' }">
+      <div class="logo" >
         <h1>Momento especial</h1>
         <h4>Ateliê</h4>
       </div>
+      </RouterLink>
       <div class="search">
         <input type="text" class="search-input" placeholder="O que você está buscando?" />
         <svg
@@ -124,29 +126,13 @@ onMounted(() => {
       </div>
     </div>
     <nav class="menu">
+    <RouterLink :to="{ name: 'Home' }">
     <button class="component-nav">Início</button>
+    </RouterLink>
     <button class="component-nav vestidos" @click="showDropdown = !showDropdown">
       <p>
         Vestidos        
       </p>
-      <svg
-          width="15px"
-          height="15px"
-          viewBox="0 -0.5 20 17"
-          version="1.1"
-          xmlns="http://www.w3.org/2000/svg"
-          xmlns:xlink="http://www.w3.org/1999/xlink"
-          class="si-glyph si-glyph-triangle-down"
-        >
-          <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-            <path
-              d="M10.106,12.69 C9.525,13.27 8.584,13.27 8.002,12.69 L1.561,6.246 C0.979,5.665 0.722,4.143 2.561,4.143 L15.549,4.143 C17.45,4.143 17.131,5.664 16.549,6.246 L10.106,12.69 L10.106,12.69 Z"
-              fill="black"
-              class="si-glyph-fill"
-            ></path>
-          </g>
-        </svg>
-     
     </button>
     <button class="component-nav">Lançamentos</button>
     <button class="component-nav">Destaques</button>
