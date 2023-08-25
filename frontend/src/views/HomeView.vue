998e5f0d7aa3d04ebf1cada1fc534336ea4868f2
carolluckmann <carolinaluckmann05@gmail.com>
2023-08-25 15:16:13 -0300

    commit

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -13,9 +13,52 @@ export default {
 }
 </script>
 <template>
-  <ul>
-    <li v-for="produto in produtos" :key="produto.id">
+  <div class="produtos">
+    <div class="card-produto" v-for="produto in produtos" :key="produto.id">
+      <img :src="produto.capa.file" alt="" />
       <router-link :to="'/produto/' + produto.id_produto">{{ produto.nome }}</router-link>
-    </li>
-  </ul>
+    </div>
+  </div>
 </template>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Lato&display=swap');
+
+h1{
+    font-family: 'Lato', sans-serif;
+    margin-left: 5%;
+    padding: 5px;
+    font-size: 28px;
+    margin-top: 20px;
+}
+hr{
+    background-color: black !important;
+    height: 2px;
+    width:90%;
+    margin-left: 5%;
+}
+.produtos {
+  /* margin: 200px; */
+  margin-top: 60px;
+  margin-left: 10%;
+  display: grid;
+  grid-template-columns: repeat(3, 33%);
+  /* grid-auto-rows: 50%; */
+}
+.card-produto {
+  width: 250px;
+  height: 430px;
+  border-radius: 6px;
+  box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 130px;
+}
+img{
+  width: 200px;
+  height: 280px;
+  border-radius: 6px;
+  margin-top: 15px;
+}
+</style>
