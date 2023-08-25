f976cad9a453041e5e2561903b44220e81f3c267
LucasGabrielAntonete <lucasantonete@gmail.com>
2023-08-25 12:02:47 -0300

    install npm

--- backend/momentoespecial/serializers/compra.py
+++ backend/momentoespecial/serializers/compra.py
@@ -1,19 +1,24 @@
 from rest_framework.serializers import ModelSerializer, CharField
 
-from momentoespecial.models import Compra, ItensCompra
+from momentoespecial.models import Compra, ItensCompra, produto
 
 
 class ItensCompraSerializer(ModelSerializer):
+    total = SerializerMethodField()
     class Meta:
         model = ItensCompra
-        fields = ["produto"] 
+        fields = ["produtos"] 
         depth = 1   
+
+    def get_total(self, obj):
+     return obj.produto.preco
 
 class CompraSerializer(ModelSerializer):
     usuario = CharField(source="usuario.email", read_only=True)
     status = CharField(source="get_status_display", read_only=True)
     itens = ItensCompraSerializer(many=True, read_only=True)
     
+
     class Meta:
         model = Compra
-        fields = "__all__"
+        fields = ("id", "usuario", "status", "total", "itens")
